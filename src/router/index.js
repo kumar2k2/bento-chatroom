2beaf4ce48df23cc205068ec8a42ac2828eb0139
Arthorn kittinukul <joker.arthorn@gmail.com>
2019-05-06 00:26:13 +0700

    [Edited] index.js es6 format

--- src/router/index.js
+++ src/router/index.js
@@ -1,59 +1,65 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Head from 'vue-head'
-import Home from '@/views/Home'
-import CheckLogin from '@/views/CheckLogin'
-import { isNil } from 'lodash'
-import store from '@/store'
+import Vue from "vue";
+import Router from "vue-router";
+import Head from "vue-head";
+import Home from "@/views/Home";
+import CheckLogin from "@/views/CheckLogin";
+import { isNil } from "lodash";
+import store from "@/store";
 
-Vue.use(Router)
+Vue.use(Router);
 
-/* If you don't know about VueHead, please refer to https://github.com/ktquez/vue-head */
+/* If you don"t know about VueHead, please refer to https://github.com/ktquez/vue-head */
 
 Vue.use(Head, {
-  complement: 'bento-starter'
-})
+  complement: "bento-starter"
+});
 
-/* If you don't know about VueRouter, please refer to https://router.vuejs.org/ */
+/* If you don"t know about VueRouter, please refer to https://router.vuejs.org/ */
 
 const router = new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/home',
-      name: 'home',
+      path: "/home",
+      name: "home",
       component: Home,
       meta: {
         authNotRequired: true
       }
     },
     {
-      path: '/check-login',
-      name: 'check-login',
+      path: "/check-login",
+      name: "check-login",
       component: CheckLogin,
       meta: {
         authNotRequired: true
       }
     },
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
       component: () =>
-        import(/* webpackChunkName: "client-chunk-login" */ '@/views/Login.vue'),
+        import(/* webpackChunkName: "client-chunk-login" */ "@/views/Login.vue"),
       meta: {
         authNotRequired: true
       }
     },
     {
-      path: '/products',
-      name: 'products',
+      path: "/products",
+      name: "products",
       component: () =>
-        import(/* webpackChunkName: "client-chunk-products" */ '@/views/Products.vue')
+        import(/* webpackChunkName: "client-chunk-products" */ "@/views/Products.vue")
     },
-    { path: '*', redirect: '/home' }
+    {
+      path: "/chat",
+      name: "chat",
+      component: () =>
+        import(/* webpackChunkName: "client-chunk-chat" */ "@/views/Chat.vue")
+    },
+    { path: "*", redirect: "/home" }
   ]
-})
+});
 
 /**
  * Handle user redirections
@@ -64,10 +70,10 @@ router.beforeEach((to, from, next) => {
     isNil(store.state.authentication.user)
   ) {
     const path =
-      store.state.authentication.user === null ? '/login' : '/check-login'
-    return next(`${path}?redirectUrl=${to.path}`)
+      store.state.authentication.user === null ? "/login" : "/check-login";
+    return next(`${path}?redirectUrl=${to.path}`);
   }
-  next()
-})
+  next();
+});
 
-export default router
+export default router;
